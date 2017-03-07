496de52a15c6ddf2b4b0a30e654f34dc820d5572
Jesus Hilario <jesushilariohernandez@gmail.com>
2017-03-06 21:46:07 -0600

    modified

--- index.js
+++ index.js
@@ -1,52 +1,39 @@
 (function () {
     // Global variables
-    const usersSearchedWord = document.getElementById("searchBox");
+    const searchBox = document.getElementById("searchBox");
+    var httpRequest;
 
     // Add random wiki page api request as an event lister
     document.getElementById("randomLink").addEventListener("click", handleRandomWikiAPI);
     // Add keyup listenr to request api data about wiki articles
     document.getElementById("searchBox").addEventListener("keyup", usersValue);
-    // Add click listenr to usersSearchedWord
-    usersSearchedWord.addEventListener("click", usersValue);
+    // Add click listenr to searchBox
+    searchBox.addEventListener("click", usersValue);
 
     //***********************************************************
-    // Function userValue recives user input and then adds that *
+    // Function usersValue recives user input and then adds that *
     // value to the api URL string                              *
     //***********************************************************
-    function userValue() {
-        let searchedWord = usersSearchedWord.value,
-            wikiURL = ""
+    function usersValue() {
+        let searchedWord = searchBox.value,
+            URLwithoutSearchWord = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=",
+            URLwithSearchedWord = URLwithoutSearchWord + searchedWord;
 
+        handleRandomWikiAPI(URLwithSearchedWord);
     }
 
-    //***********************************************
-    // handleRandomWikiAPI Function handles the api request   *
-    // for forismatic to send us a random quote     *
-    //***********************************************
-    function handleRandomWikiAPI() {
+    //*******************************************************
+    // handleRandomWikiAPI Function handles the api request *
+    // for wikipedia to send us a random quote              *
+    //*******************************************************
+    function handleRandomWikiAPI(url) {
         // Local variables
-        let httpRequest = new XMLHttpRequest(),
-            method = "GET",
-            url = "https://en.wikipedia.org/w/api/php?action=opensearch&format=json&callback=?search=";
+        httpRequest = new XMLHttpRequest();
+        const method = "POST";
 
         // call (open, readystate, and send) methods
         httpRequest.open(method, url, true);
-        httpRequest.onreadystatechange = requestStatus;
-        httpRequest.send();
-    }
-
-    //***********************************************
-    // handleRandomWikiAPI Function handles the api request   *
-    // for forismatic to send us a random quote     *
-    //***********************************************
-    function handleRandomWikiAPI() {
-        // Local variables
-        let httpRequest = new XMLHttpRequest(),
-            method = "GET",
-            url = "https://en.wikipedia.org/w/api/php?action=opensearch&format=json&callback=?search=";
-
-        // call (open, readystate, and send) methods
-        httpRequest.open(method, url, true);
+        httpRequest.setRequestHeader( 'Api-User-Agent', 'Example/1.0' );
         httpRequest.onreadystatechange = requestStatus;
         httpRequest.send();
     }
@@ -56,10 +43,9 @@
     //* when the document is read                                  *
     //**************************************************************
     function requestStatus() {
-        if (this.requestStatus === XMLHttpRequest.DONE && this.status === 200) {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
             /* add stuff to the document */
-        } else {
-            /* add event handler, that an error has occured */
+            console.log(this.responseText);
         }
     }
 
