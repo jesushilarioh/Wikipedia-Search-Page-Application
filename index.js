0f24a4c3399be2c5e911e64acba38cc037cf64a5
Jesus Hilario <jesushilariohernandez@gmail.com>
2017-03-16 23:43:21 -0500

    modified.

--- index.js
+++ index.js
@@ -1,68 +1,79 @@
-(function () {
-    // Global variables
-    const searchBox = document.getElementById("searchBox");
-    var httpRequest;
+$(document).ready(function() {
+    const wikiLinksElement = $("#wiki-links");
+    //random  wiki button
+    $("#random").click(function() {
+        window.open("https://en.wikipedia.org/wiki/Special:Random");
+    });
+    //search URL
+    //"https://www.mediawiki.org/wiki/API:Main_page"
+    function updateLinks(data) {
+        var topic, desc, link, resultHtml = "";
+        // 1 - Topic, 2 = Short desc, 3 = Links
 
-    // Add random wiki page api request as an event lister
-    document.getElementById("randomLink").addEventListener("click", handleRandomWikiAPI);
-    // Add keyup listenr to request api data about wiki articles
-    document.getElementById("searchBox").addEventListener("keyup", usersValue);
-    // Add click listenr to searchBox
-    searchBox.addEventListener("click", usersValue);
+        if (data[1] !== undefined) {
+            for (var i = 0; i < data[1].length; i++) {
+                topic = data[1][i];
+                desc = data[2][i];
+                link = data[3][i];
 
-    //***********************************************************
-    // Function usersValue recives user input and then adds that *
-    // value to the api URL string                              *
-    //***********************************************************
-    function usersValue() {
-        let searchedWord = searchBox.value,
-            URLwithoutSearchWord = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=",
-            URLwithSearchedWord = URLwithoutSearchWord + searchedWord;
-
-        handleRandomWikiAPI(URLwithSearchedWord);
-    }
-
-    //*******************************************************
-    // handleRandomWikiAPI Function handles the api request *
-    // for wikipedia to send us a random quote              *
-    //*******************************************************
-    function handleRandomWikiAPI(url) {
-        // Local variables
-        httpRequest = new XMLHttpRequest();
-        const method = "POST";
-
-        // call (open, readystate, and send) methods
-        httpRequest.open(method, url, true);
-        httpRequest.setRequestHeader( 'Api-User-Agent', 'Example/1.0' );
-        httpRequest.onreadystatechange = requestStatus;
-        httpRequest.send();
-    }
-
-    //**************************************************************
-    //* requestStatus function handles the actions to be perfomed  *
-    //* when the document is read                                  *
-    //**************************************************************
-    function requestStatus() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            /* add stuff to the document */
-            console.log(this.responseText);
+                var content = "";
+                content += "<div class='well' onClick='window.open(\"" + link + "\");'>";
+                content += "\t<h4>" + topic + "</h4>";
+                content += "\t<p class='wiki-link-desc'>" + desc + "</p>";
+                content += "</div>";
+                resultHtml += content;
+            }
+            wikiLinksElement.html(resultHtml);
+        } else {
+            resultHtml = "";
+            wikiLinksElement.html(resultHtml);
         }
     }
 
-    //***************************************************************
-    // iframeContent function added content to the iframe element   *
-    // in the main html document                                    *
-    //***************************************************************
-    function iframeContent() {
+    function search() {
+        var ENG_WIKI_API_LINK = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=",
+            textToSearch = $("#input-search").val(),
+            request = ENG_WIKI_API_LINK + textToSearch;
 
+        $.ajax({
+            type: "POST",
+            url: request,
+            dataType: "jsonp",
+            success: function(data, textStatus, jqXHR) {
+                updateLinks(data);
+            },
+        });
     }
 
-    //*******************************************************************
-    // displaySuggestedArticles function displays suggested articles.   *
-    // Each article is assigned it's own individual well.               *
-    //*******************************************************************
-    function displaySuggestedArticles() {
+    function handleSearch() {
+        search();
 
+        if (wikiLinksElement.css('visibility') === 'hidden') {
+            wikiLinksElement.css('visibility', 'visible');
+            $("#wiki-links").html();
+        }
     }
 
-})();
+    // Add click event to clear button
+    $("#search-clear").on('click', function() {
+        $("#input-search").val('');
+
+        // hide wiki-links
+        if (wikiLinksElement.css('visibility') === 'visible') {
+            wikiLinksElement.css('visibility', 'hidden');
+        }
+
+        // Clear wiki-links content
+        wikiLinksElement.html();
+    });
+
+    // Add Click Event to search-button
+    $("#search-button").on('click', function() {
+        handleSearch();
+    });
+
+    // Add Keyup Event to input-button
+    $("#input-search").on('keyup', function() {
+        handleSearch();
+    });
+});
